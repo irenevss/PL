934fb332f90a639af999674c3f6b1b8b79fd60d2
Irene <ivalve02@ucm.es>
2026-02-08 19:21:54 +0100

    Analizador Lexico parte Irene

---
+++ src/main/java/fdi/Estado.java
@@ -0,0 +1,40 @@
+package fdi;
+
+public enum Estado {
+    INICIO,
+
+    REC_MUL,
+    REC_AND,
+    REC_OR,
+    REC_DIV,
+    REC_NOT,
+    REC_IGUAL,
+    REC_MENOR,
+    REC_MENOR_IGUAL,
+    REC_DISTINTO,
+    REC_MAYOR,
+    REC_MAYOR_IGUAL,
+
+    REC_PUNTOCOMA,
+    REC_DOSPUNTOS,
+    REC_ASIG,
+    REC_PAP,
+    REC_PCIE,
+
+    REC_ID,
+    REC_MAS,
+    REC_ENT,
+    REC_MENOS,
+    REC_DOBLEGUION,
+    REC_0,
+    REC_IDEC,
+    REC_DEC,
+    REC_0DEC,
+
+    REC_EXP,
+    REC_EXP0,
+    REC_EXP_SIGNO,
+    REC_EXP_ENT,
+
+    EOF
+}
